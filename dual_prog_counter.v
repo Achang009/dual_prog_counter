f4d26acc9154697603b3ae92ae7e367955ced5a9
Achang009 <146834990+Achang009@users.noreply.github.com>
2026-01-15 00:43:54 +0800

    Update dual_prog_counter.v

--- dual_prog_counter.v
+++ dual_prog_counter.v
@@ -3,11 +3,14 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity updown_counter_dynamic is
+    Generic (
+        g_DIV_RATIO : integer := 50000000 
+    );
     Port (
-        i_clk        : in  STD_LOGIC;
-        i_reset      : in  STD_LOGIC;
-        i_min        : in  STD_LOGIC_VECTOR(3 downto 0);
-        i_max        : in  STD_LOGIC_VECTOR(3 downto 0); 
+        i_clk       : in  STD_LOGIC;
+        i_reset     : in  STD_LOGIC;
+        i_min       : in  STD_LOGIC_VECTOR(3 downto 0);
+        i_max       : in  STD_LOGIC_VECTOR(3 downto 0); 
         o_countup   : out STD_LOGIC_VECTOR(3 downto 0);
         o_countdown : out STD_LOGIC_VECTOR(3 downto 0)
     );
@@ -15,39 +18,95 @@ end updown_counter_dynamic;
 
 architecture Behavioral of updown_counter_dynamic is
     
-    signal r_cntup   : unsigned(3 downto 0) := (others => '0');
-    signal r_cntdown : unsigned(3 downto 0) := (others => '0');
+    signal cntup     : unsigned(3 downto 0) := (others => '0');
+    signal cntdown   : unsigned(3 downto 0) := (others => '0');
+    signal r_div_cnt : integer range 0 to g_DIV_RATIO - 1 := 0;
+    signal w_tick    : std_logic := '0';
 
 begin
-
-    UP_COUNTER: process(i_clk, i_reset)
+    freq: process(i_clk, i_reset)
     begin
         if i_reset = '1' then
-            r_cntup <= unsigned(i_min);
+            r_div_cnt <= 0;
+            w_tick    <= '0';
         elsif rising_edge(i_clk) then
-            if r_cntup >= unsigned(i_max) then
-                r_cntup <= unsigned(i_min);
+            if r_div_cnt = g_DIV_RATIO - 1 then
+                r_div_cnt <= 0;
+                w_tick    <= '1';
             else
-                r_cntup <= r_cntup + 1;
+                r_div_cnt <= r_div_cnt + 1;
+                w_tick    <= '0';
             end if;
         end if;
-    end process UP_COUNTER;
+    end process freq;
 
-    DOWN_COUNTER: process(i_clk, i_reset)
+    up_counter: process(i_clk, i_reset)
     begin
         if i_reset = '1' then
-            r_cntdown <= unsigned(i_max);
+            cntup <= unsigned(i_min);
         elsif rising_edge(i_clk) then
-            if r_cntdown <= unsigned(i_min) then
-                r_cntdown <= unsigned(i_max);
-            else
-                r_cntdown <= r_cntdown - 1;
+            if w_tick = '1' then
+                if cntup >= unsigned(i_max) then
+                    cntup <= unsigned(i_min);
+                else
+                    cntup <= cntup + 1;
+                end if;
             end if;
         end if;
-    end process DOWN_COUNTER;
-    OUTPUT: process(r_cntup, r_cntdown)
+    end process up_counter;
+
+    down_counter: process(i_clk, i_reset)
     begin
-        o_countup   <= std_logic_vector(r_cntup);
-        o_countdown <= std_logic_vector(r_cntdown);
-    end process OUTPUT;
+        if i_reset = '1' then
+            cntdown <= unsigned(i_max);
+        elsif rising_edge(i_clk) then
+            if w_tick = '1' then
+                if cntdown <= unsigned(i_min) then
+                    cntdown <= unsigned(i_max);
+                else
+                    cntdown <= cntdown - 1;
+                end if;
+            end if;
+        end if;
+    end process down_counter;
+    o_countup   <= std_logic_vector(cntup);
+    o_countdown <= std_logic_vector(cntdown);
 end Behavioral;
+
+接腳設定
+set_property PACKAGE_PIN F22 [get_ports {i_max[3]}]
+set_property PACKAGE_PIN G22 [get_ports {i_max[2]}]
+set_property PACKAGE_PIN H22 [get_ports {i_max[1]}]
+set_property PACKAGE_PIN F21 [get_ports {i_max[0]}]
+set_property PACKAGE_PIN H19 [get_ports {i_min[3]}]
+set_property PACKAGE_PIN H18 [get_ports {i_min[2]}]
+set_property PACKAGE_PIN H17 [get_ports {i_min[1]}]
+set_property PACKAGE_PIN M15 [get_ports {i_min[0]}]
+set_property PACKAGE_PIN T22 [get_ports {o_countdown[3]}]
+set_property PACKAGE_PIN T21 [get_ports {o_countdown[2]}]
+set_property PACKAGE_PIN U22 [get_ports {o_countdown[1]}]
+set_property PACKAGE_PIN U21 [get_ports {o_countdown[0]}]
+set_property PACKAGE_PIN V22 [get_ports {o_countup[3]}]
+set_property PACKAGE_PIN W22 [get_ports {o_countup[2]}]
+set_property PACKAGE_PIN U19 [get_ports {o_countup[1]}]
+set_property PACKAGE_PIN U14 [get_ports {o_countup[0]}]
+set_property PACKAGE_PIN Y9 [get_ports i_clk]
+set_property PACKAGE_PIN P16 [get_ports i_reset]
+set_property IOSTANDARD LVCMOS33 [get_ports {i_max[3]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {i_max[2]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {i_max[1]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {i_max[0]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {i_min[3]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {i_min[2]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {i_min[1]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {i_min[0]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {o_countup[3]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {o_countup[2]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {o_countup[1]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {o_countup[0]}]
+set_property IOSTANDARD LVCMOS33 [get_ports i_clk]
+set_property IOSTANDARD LVCMOS33 [get_ports i_reset]
+set_property IOSTANDARD LVCMOS33 [get_ports {o_countdown[3]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {o_countdown[2]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {o_countdown[1]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {o_countdown[0]}]
